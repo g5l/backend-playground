9bb6b2ab69787cd31827f897e60ed4f548979701
Gabriel Debona <gabriel.debona@gmail.com>
2026-07-28 10:00:00 -0300

    add BookRepository class with in-memory book data

---
+++ java/graphql/src/main/java/poc/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package poc.repository;
+
+import poc.domain.Book;
+
+import java.util.List;
+
+public class BookRepository {
+
+    private final List<Book> books = List.of(
+            new Book("b1", "A Wizard of Earthsea", "a1"),
+            new Book("b2", "The Dispossessed", "a1"),
+            new Book("b3", "Dom Casmurro", "a2")
+    );
+}
