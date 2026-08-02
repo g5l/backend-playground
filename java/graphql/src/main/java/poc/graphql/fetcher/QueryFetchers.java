1e5f2c58b8b8d21c263f4fd557629e8653bf9c7e
Gabriel Debona <gabriel.debona@gmail.com>
2026-08-02 10:00:00 -0300

    add QueryFetchers.books() root query fetcher

---
+++ java/graphql/src/main/java/poc/graphql/fetcher/QueryFetchers.java
@@ -0,0 +1,20 @@
+package poc.graphql.fetcher;
+
+import graphql.schema.DataFetcher;
+import poc.domain.Book;
+import poc.repository.BookRepository;
+
+import java.util.List;
+
+public class QueryFetchers {
+
+    private final BookRepository bookRepository;
+
+    public QueryFetchers(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public DataFetcher<List<Book>> books() {
+        return env -> bookRepository.findAll();
+    }
+}
